f8a73f273084a1e6456f49324550c0ab881e9dd8
Yura Simonov <yusimo@yandex.ru>
2026-03-26 23:56:53 +0300

    - Приложение sights добавлено в settings.py; - Добавлена модель Категория; - Созданы и применены миграции; - Обновлен .gitignore

--- sights/models.py
+++ sights/models.py
@@ -1,3 +1,15 @@
+from users.models import NULLABLE
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField('Название', max_length=100)
+    description = models.TextField('Описание', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['name']
