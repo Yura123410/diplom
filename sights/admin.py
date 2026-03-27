ef0acad5323eb4e4c3bfdea46d8ad9ab7b2d46c7
Yura Simonov <yusimo@yandex.ru>
2026-03-27 23:45:32 +0300

    - Добавлены настройки для STATIC и MEDIA; - Добавлены пути для STATIC и MEDIA; - Добавлены пути к приложению sights

--- sights/admin.py
+++ sights/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, Sight
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name',]
+
+
+@admin.register(Sight)
+class SightAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'address', 'views_count','created_at']
+    list_filter = ['category','created_at']
+    readonly_fields = ['views_count']
+    fieldsets = (
+        ('Основная информация', {
+            'fields': ('name', 'category', 'short_description', 'full_description')
+        }),
+        ('Локация', {
+            'fields': ('address',)
+        }),
+        ('Медиа', {
+            'fields': ('image',)
+        }),
+        ('Дополнительно', {
+            'fields': ('opening_hours', 'ticket_price', 'views_count')
+        }),
+    )
