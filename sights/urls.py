10d9fa724cb34acddca3f88104e29d16b075c1bd
Yura Simonov <yusimo@yandex.ru>
2026-03-28 00:58:02 +0300

    - Написаны sight_detail view и sight_list view; - Написаны urls для sight_detail, sight_list; - Добавлены шаблоны для sight_detail, sight_list;

--- sights/urls.py
+++ sights/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from sights.views import index
+from sights.views import index, sight_list, sight_detail
 from sights.apps import SightsConfig
 
 app_name = SightsConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
+    path('sights/', sight_list, name='sight'),
+    path('sights/<int:pk>/sight_detail/',sight_detail, name='sight_detail'),
 ]
--- sights/views.py
+++ sights/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from sights.models import Sight, Category
 
@@ -9,3 +9,18 @@ def index(request):
         'title': 'Главная - Достопримечательности Мурманска',
     }
     return render(request, 'sights/index.html', context)
+
+def sight_list(request):
+    context = {
+        'objects_list': Sight.objects.all(),
+        'title': 'Все достопримечательности',
+    }
+    return render(request, 'sights/sight_list.html', context)
+
+def sight_detail(request, pk: int):
+    sight = get_object_or_404(Sight, pk=pk)
+    context = {
+        'sight': sight,
+        'title': sight.name,
+    }
+    return render(request, 'sights/sight_detail.html', context)
