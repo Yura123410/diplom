3216e728ee57e13ce4ca088770e3c3f23b1c6543
Yura Simonov <yusimo@yandex.ru>
2026-04-02 00:29:33 +0300

    - Добавлен category_list и category_detail; - Добавлены urls.

--- sights/urls.py
+++ sights/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
-from sights.views import index, sight_list, sight_detail, sight_create_view
+from sights.views import index, sight_list, sight_detail, sight_create_view, category_list, category_detail
 from sights.apps import SightsConfig
 
 app_name = SightsConfig.name
 
 urlpatterns = [
     path('', index, name='index'),
-    path('sights/', sight_list, name='sight'),
-    path('sights/<int:pk>/sight_detail/',sight_detail, name='sight_detail'),
+    path('sights/', sight_list, name='sight_list'),
+    path('sights/<int:pk>/sight_detail/', sight_detail, name='sight_detail'),
     path('sights/create/', sight_create_view, name='sight_create'),
+    path('category/', category_list, name='category_list'),
+    path('category/<int:pk>/', category_detail, name='category'),
 ]
--- sights/views.py
+++ sights/views.py
@@ -8,10 +8,11 @@ from sights.models import Sight, Category
 
 def index(request):
     context = {
-        'objects_list': Category.objects.all()[:3],
+        'objects': Category.objects.all()[:3],
         'title': 'Мурманск - Главная',
     }
     return render(request, 'sights/index.html', context)
+
 
 def sight_list(request):
     context = {
@@ -19,6 +20,15 @@ def sight_list(request):
         'title': 'Все достопримечательности',
     }
     return render(request, 'sights/sight_list.html', context)
+
+
+def category_list(request):
+    context = {
+        'objects_list': Category.objects.all(),
+        'title': 'Категории',
+    }
+    return render(request, 'sights/category_list.html', context)
+
 
 def sight_detail(request, pk: int):
     sight = get_object_or_404(Sight, pk=pk)
@@ -28,16 +38,27 @@ def sight_detail(request, pk: int):
     }
     return render(request, 'sights/sight_detail.html', context)
 
+
 def sight_create_view(request):
     if request.method == 'POST':
         form = SightForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return HttpResponseRedirect(reverse('sights/sight_list.html'))
+            return HttpResponseRedirect(reverse('sight_list.html'))
     context = {
         'title': 'Добавить достопримечательность',
         'form': SightForm()
     }
-    return render(request, 'sights/create.html', )
+    return render(request, 'sights/create.html', context)
 
 
+def category_detail(request, pk: int):
+    category = get_object_or_404(Category, pk=pk)
+    sights = Sight.objects.filter(category=category)
+
+    context = {
+        'category': category,
+        'sights': sights,
+        'title': category.name,
+    }
+    return render(request, 'sights/category_detail.html', context)
