3d6acf87fb263fd7180988dfde11ad27afcbc5da
Yura Simonov <yusimo@yandex.ru>
2026-04-10 00:50:20 +0300

    - Добавлен функционал авторизации пользователя

--- users/views.py
+++ users/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth import authenticate, login, logout
 
-from users.forms import UserRegisterForm
+from users.forms import UserRegisterForm, UserLoginForm
 
 def user_register_view(request):
     if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             new_user = form.save()
-            print(new_user)
-            print(form.cleaned_data['password'])
             new_user.set_password(form.cleaned_data['password'])
             new_user.save()
             return HttpResponseRedirect(reverse('sights:index'))
@@ -22,3 +21,22 @@ def user_register_view(request):
         'form': form
     }
     return render(request, 'users/user_register.html', context=context)
+
+
+def user_login_view(request):
+    if request.method == 'POST':
+        form = UserLoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(email=cd['email'], password=cd['password'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return  HttpResponseRedirect(reverse('sights:index'))
+                return HttpResponse('Аккаунт неактивен')
+            return HttpResponse('Нет такого пользователя!')
+    context = {
+        'title': 'Авторизация',
+        'form': UserLoginForm
+    }
+    return render(request, 'users/user_login.html', context=context)
